b19fe50101def7e8c93c602f2a9a5fdea3ea658a
dlsmallw <dlsmallw@asu.edu>
2024-10-17 13:52:55 +0900

    Task-56 Practice configuring separate windows

--- dlsmallw/ElectronJS Practice/main.js
+++ dlsmallw/ElectronJS Practice/main.js
@@ -1,29 +1,65 @@
 // Primary entry point for the application
 
-const { app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('node:path');
 
-const path = require('node:path')
+const isMac = process.platform === 'darwin';
+const isDev = process.env.NODE_ENV !== 'production';
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
+        width: isDev ? 1200 : 800,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     })
 
+    if (isDev) {
+        win.webContents.openDevTools();
+    }
+
     win.loadFile('index.html')
 }
+
+const createAboutWindow = () => {
+    const aboutWin = new BrowserWindow({
+        title: 'About Window',
+        width: 800,
+        height: 600
+    })
+
+    aboutWin.loadFile('about.html')
+}
+
+const menu = [
+    {
+        label: app.name,
+        submenu: [
+            {
+                label: 'About',
+                click: createAboutWindow
+            }
+        ]
+    },
+    {
+        role: 'fileMenu',
+    },
+]
 
 app.whenReady().then(() => {
     createWindow()
 
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') app.quit()
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+
+
+app.on('window-all-closed', () => {
+    if (!isMac) app.quit()
 })
