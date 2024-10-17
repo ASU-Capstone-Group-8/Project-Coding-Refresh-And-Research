22dea768eb9404925481a44d2cb3da71ed978dfe
dlsmallw <dlsmallw@asu.edu>
2024-10-17 12:06:16 +0900

    Task-56 Practice using a renderer script for updating DOM

---
+++ dlsmallw/ElectronJS Practice/renderer.js
@@ -0,0 +1,8 @@
+const nodeVersion = document.getElementById('node-version')
+nodeVersion.innerText = versions.node()
+
+const chromeVersion = document.getElementById('chrome-version')
+chromeVersion.innerHTML = versions.chrome()
+
+const electronVersion = document.getElementById('electron-version')
+electronVersion.innerHTML = versions.electron()
